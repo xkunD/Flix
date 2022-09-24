431d88940d502420173f327e537f9fd084a5134e
xkunD <103330637+xkunD@users.noreply.github.com>
2022-09-24 01:15:57 +0100

    Part 2 Uploaded

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Xiaokun Du on 24/09/2022.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -25,8 +25,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
         
-        print("HEllo")
-        
+  
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -42,7 +41,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 
                 self.tableView.reloadData()
                 
-                print(dataDictionary)
+   
                 
 
                     // TODO: Get the array of movies
@@ -77,6 +76,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.posterView.af.setImage(withURL: posterUrl!)
         
         return cell;
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("loading up the details screen")
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
 
